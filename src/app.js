1e0a2beef26442da13040642af3990cc0911be5a
Agus458 <aguspe458@outlook.com>
2021-04-22 12:26:46 +0000

    creation of random card generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+let figure = ["♦", "♥", "♠", "♣"];
+
+function generateRandom(rango) {
+  return Math.floor(Math.random() * rango);
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let card = document.querySelector(".card");
+  let number = document.querySelector(".number");
+  let figures = document.getElementsByClassName("figure");
+
+  number.innerHTML = numbers[generateRandom(numbers.length)];
+  let color = generateRandom(figure.length);
+  figures[0].innerHTML = figure[color];
+  figures[1].innerHTML = figure[color];
+
+  if (figure[color] == "♦" || figure[color] == "♥") {
+    card.classList.add("red");
+  } else {
+    card.classList.remove("red");
+  }
 };
